dde8b178717679ac254ad40a28272ead0b6d8d54
Anurag Pawar <114361618+toxicanurag@users.noreply.github.com>
2025-05-07 07:34:14 +0530

    Create StudentService.java

---
+++ src/main/Java/com.example.studentcourse/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.example.studentcourse.service;
+
+import com.example.studentcourse.entity.Student;
+import com.example.studentcourse.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void updateStudent(Student student) {
+        studentRepository.save(student);
+    }
+}
